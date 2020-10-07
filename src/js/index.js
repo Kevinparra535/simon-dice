3992aa4c04c650a8abb71b76051a749bf487b327
Kevin Parra <53342165+Kevinparra535@users.noreply.github.com>
2020-10-07 10:47:31 -0500

    Generando una secuencia de números

--- src/js/index.js
+++ src/js/index.js
@@ -6,14 +6,28 @@ const btnEmpezar = document.getElementById("btnEmpezar");
 
 class Game {
   constructor() {
-    this.init()
+    this.init();
+    this.generateSecuency();
   }
 
   init() {
-      btnEmpezar.classList.add('hide')
+    btnEmpezar.classList.add("hide");
+    this.nivel = 1;
+    this.colors = {
+      celeste,
+      violeta,
+      naranja,
+      verde,
+    };
+  }
+
+  generateSecuency() {
+    this.secuency = new Array(10)
+      .fill(0)
+      .map((n) => Math.floor(Math.random() * 4));
   }
 }
 
 function empezarJuego() {
-  var game = new Game()
+  window.game = new Game();
 }
