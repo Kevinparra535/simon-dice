23a48364934120ef30bee2290900c249eee5d9e9
Kevin Parra <53342165+Kevinparra535@users.noreply.github.com>
2020-10-07 11:16:20 -0500

    Iluminando la secuencia de colores

--- src/js/index.js
+++ src/js/index.js
@@ -8,11 +8,12 @@ class Game {
   constructor() {
     this.init();
     this.generateSecuency();
+    this.nextLevel();
   }
 
   init() {
     btnEmpezar.classList.add("hide");
-    this.nivel = 1;
+    this.level = 1;
     this.colors = {
       celeste,
       violeta,
@@ -25,6 +26,39 @@ class Game {
     this.secuency = new Array(10)
       .fill(0)
       .map((n) => Math.floor(Math.random() * 4));
+  }
+
+  nextLevel() {
+    this.illuminateSequence();
+  }
+
+  transformNumToColor(number) {
+    switch (number) {
+      case 0:
+        return "celeste";
+      case 1:
+        return "violeta";
+      case 2:
+        return "naranja";
+      case 3:
+        return "verde";
+    }
+  }
+
+  illuminateSequence() {
+    for (let i = 0; i < this.level; i++) {
+      const color = this.transformNumToColor(this.secuency[i]);
+      setTimeout(() => this.illuminateColor(color), 1000 * i);
+    }
+  }
+
+  illuminateColor(color) {
+    this.colors[color].classList.add("light");
+    setTimeout(() => this.offColor(color), 350);
+  }
+
+  offColor(color) {
+    this.colors[color].classList.remove("light");
   }
 }
 
